8ac379b918704093e08643f00c9924e1d3221fa9
VJoss <verdelet.jossua@gmail.com>
2024-03-19 16:50:10 +0100

    onclick pour ouvivr les liens dans une nouvelle page

--- src/components/PortefolioItem.js
+++ src/components/PortefolioItem.js
@@ -8,7 +8,7 @@ function PortefolioItem () {
     useEffect(() => {
         setPortefolioData(jsonData);
     }, []);
-    const openProjectSite = (link) => {
+    const openSite = (link) => {
         window.open(link, "_blank"); // Ouvre le lien dans une nouvelle fenêtre ou un nouvel onglet
     };
 
@@ -23,7 +23,7 @@ function PortefolioItem () {
                 <h3 id="projets">Projets</h3>
                 <ul className="bloc-projets">
                     {PortefolioData.projects.map((project, index) => (
-                        <li className="carte-projet"key={index} onClick={() => openProjectSite(project.link)}>
+                        <li className="carte-projet"key={index} onClick={() => openSite(project.link)}>
                             <h4>{project.title}</h4>
                             <p>{project.description}</p>
                             
@@ -35,8 +35,8 @@ function PortefolioItem () {
             )}
 
 <div className="social-icons">
-            <a className="social-icon" href="https://www.linkedin.com/in/jossua-verdelet-1052bb257/"><FaLinkedin /></a>
-            <a className="social-icon" href="https://github.com/VJoss"><FaGithub /></a>
+            <a className="social-icon" onClick={() => openSite("https://www.linkedin.com/in/jossua-verdelet-1052bb257/")}><FaLinkedin /></a>
+            <a className="social-icon" onClick={() => openSite("https://github.com/VJoss")}><FaGithub /></a>
         </div>
         </div>
     );
